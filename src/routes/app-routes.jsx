2aa237d5739e3857718a4958b3c8f06cab0f5d30
Riccardo Biffi <biffi.riccardo91@gmail.com>
2025-01-27 16:33:13 -0600

    Add invoice route

--- src/routes/app-routes.jsx
+++ src/routes/app-routes.jsx
@@ -1,11 +1,13 @@
 import Home from "@/pages/home";
 import Onboarding from "@/pages/onboarding";
 import { WalletInfo } from "@/pages/wallet-info";
+import { Invoice } from "@/components/invoice";
 import { Switch, Route } from "react-router-dom";
 
 const AppRoutes = () => (
   <Switch>
     <Route path="/wallet" component={WalletInfo} />
+    <Route path="/invoice" component={Invoice} />
     <Route path="/home" component={Home} />
     <Route path="/" component={Onboarding} />
   </Switch>
