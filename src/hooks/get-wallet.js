0e60eff247f7c100fcc7a80fa6ec2e198fc42e53
sljuka <david.sljukic.dev@gmail.com>
2025-01-27 19:47:01 +0100

    use react-quer

--- src/hooks/get-wallet.js
+++ src/hooks/get-wallet.js
@@ -1,38 +1,24 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const useWalletInfo = () => {
-    const API_KEY = import.meta.env.VITE_WALLET_ID;
-    const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const API_KEY = import.meta.env.VITE_WALLET_ID;
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-    const [walletInfo, setWalletInfo] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  console.log('AAAA', API_KEY, typeof API_KEY)
 
-    useEffect(() => {
-        const fetchWalletInfo = async () => {
-            try {
-                const response = await axios({
-                    url: `${BASE_URL}${"/api/v1/wallet"}`,
-                    method: "GET",
-                    headers: {
-                        'X-Api-Key': API_KEY,
-                        'Content-Type': 'application/json',
-                    },
-                    data: undefined,
-                });
-                setWalletInfo(response.data);
-            } catch (err) {
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchWalletInfo();
-    }, []);
-
-    return { walletInfo, loading, error };
+  return useQuery({
+    queryKey: ["walletInfo"],
+    queryFn: () =>
+      axios({
+        url: `${BASE_URL}${"/api/v1/wallet"}`,
+        method: "GET",
+        headers: {
+          "X-Api-Key": API_KEY,
+          "Content-Type": "application/json",
+        },
+      }),
+  });
 };
 
 export default useWalletInfo;
--- src/pages/wallet-info.jsx
+++ src/pages/wallet-info.jsx
@@ -1,16 +1,17 @@
 import useWalletInfo from "@/hooks/get-wallet"
 
 export const WalletInfo = () => {
-    const { walletInfo, loading, error } = useWalletInfo()
+    const { data: walletInfo, loading, error } = useWalletInfo()
 
+    console.log('AAA', walletInfo)
     if (loading) return <div>Loading</div>
 
-    if (error) return <div>{error}</div>
+    if (error) return <div>Error</div>
 
     return (
         <div className='pt-4 flex gap-4 flex-col'>
-            <span>Balance: {walletInfo.balance}</span>
-            <span>Name: {walletInfo.name}</span>
+            {/* <span>Balance: {walletInfo.balance}</span>
+            <span>Name: {walletInfo.name}</span> */}
         </div>
     )
 }
