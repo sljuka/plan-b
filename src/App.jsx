3408de65df3a362ae1cc155b692b9c4ce4afcb65
Riccardo Biffi <biffi.riccardo91@gmail.com>
2025-01-27 11:52:05 -0600

    lnbits

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,7 @@ import {
 } from "react-router-dom";
 import './App.css'
 
-import useWalletInfo from './hooks/get-wallet'
-
 function App() {
-<<<<<<< HEAD
-  const [count, setCount] = useState(0)
-  const walletId = "40c40a9400b248d0a44565adcec48567";
-  const { walletInfo, loading, error } = useWalletInfo(walletId);
-  console.log(walletInfo);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Wallet Info: {loading ? 'Loading...' : error ? error.message : walletInfo?.name + " " + walletInfo?.balance}
-        </p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-=======
   return (
     <Router>
       <form >
@@ -74,7 +39,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
->>>>>>> upstream/main
   )
 }
 
