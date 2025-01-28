a868945e6037cbc9ae8b8f4edef6007c1a850a50
Riccardo Biffi <biffi.riccardo91@gmail.com>
2025-01-28 16:38:07 -0600

    Formet home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import BitcoinBalance from "@/components/bitcoin-balance";
 import useWalletInfo from "@/hooks/get-wallet";
 
 export default function Home() {
-  const { data: walletInfo} = useWalletInfo();
+  const { data: walletInfo } = useWalletInfo();
 
   if (!walletInfo) {
     return <div className="min-h-screen bg-black">Loading...</div>;
@@ -12,8 +12,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-black text-white p-4 md:p-8">
-
-{}
+      {}
 
       <div className="max-w-4xl mx-auto">
         <header className="flex justify-between items-center mb-12">
@@ -31,10 +30,16 @@ export default function Home() {
           <section className="block">
             <h2 className="text-xl mb-6 font-light">Quick Actions</h2>
             <div className="flex gap-4 sm:gap-6">
-              <Button variant="secondary" className="flex-1 transition-all duration-300 py-4 sm:py-6 text-lg rounded-xl shadow-lg text-[16px] sm:text-[18px] hover:bg-[#f89b2adf] text-white font-normal sm:px-20">
+              <Button
+                variant="secondary"
+                className="flex-1 transition-all duration-300 py-4 sm:py-6 text-lg rounded-xl shadow-lg text-[16px] sm:text-[18px] hover:bg-[#f89b2adf] text-white font-normal sm:px-20"
+              >
                 <ArrowDownLeft className="mr-2 h-5 sm:h-6 w-5 sm:w-6" /> Receive
               </Button>
-              <Button variant="secondary" className="flex-1 text-white hover:bg-[#F89B2A]/90 transition-all duration-300 py-4 sm:py-6 text-lg rounded-xl shadow-lg sm:px-20">
+              <Button
+                variant="secondary"
+                className="flex-1 text-white hover:bg-[#F89B2A]/90 transition-all duration-300 py-4 sm:py-6 text-lg rounded-xl shadow-lg sm:px-20"
+              >
                 <ArrowUpRight className="mr-2 h-5 sm:h-6 w-5 sm:w-6" /> Send
               </Button>
             </div>
