f8fc904732eb327bed1581fb3869da3e6d8cffd3
dandev <danieldubonxd@gmail.com>
2025-01-28 11:11:28 -0600

    registration form

--- src/forms/registration-form.jsx
+++ src/forms/registration-form.jsx
@@ -1,40 +1,60 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const RegistrationForm = () => {
-  const [name, setName] = useState("");
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .required("Name cannot be empty.")
+      .max(50, "Name cannot exceed 50 characters.")
+      .min(5, "Name cannot be less than 5 characters."),
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Evitar que el formulario recargue la página
-    console.log("Name:", name); // Mostrar el nombre en la consola
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      console.log("Name:", values.name);
+      setSubmitting(false);
+    }, 1000);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name" className="text-sm font-medium">
-          Name
-        </label>
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Enter your name"
-          className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive my-3"
-          aria-errormessage="error-email"
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <Button
-          type="submit"
-          variant="secondary"
-          className=" transition-all md:w-auto w-full duration-300 rounded-xl shadow-lg hover:bg-[#f89b2adf] font-normal"
-        >
-          Submit
-        </Button>
-      </div>
-    </form>
+    <Formik
+      initialValues={{ name: "" }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="name" className="text-sm font-medium">
+              Name
+            </label>
+            <Field
+              as={Input}
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your name"
+              className="group-data-[invalid=true]/field:border-destructive focus-visible:group-data-[invalid=true]/field:ring-destructive"
+            />
+            <ErrorMessage
+              name="name"
+              component="p"
+              className="text-destructive text-sm pt-2"
+            />
+            <Button
+              type="submit"
+              variant="secondary"
+              className="mt-2 transition-all md:w-auto w-full duration-300 rounded-xl shadow-lg hover:bg-[#f89b2adf] font-normal"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
