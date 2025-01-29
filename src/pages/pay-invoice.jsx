8d0e9e0dd8aa07696d3467d00dbfee2b09b06de3
Riccardo Biffi <biffi.riccardo91@gmail.com>
2025-01-28 19:39:44 -0600

    Update payment logic for input field (invoice or lnurl)

--- src/pages/pay-invoice.jsx
+++ src/pages/pay-invoice.jsx
@@ -1,13 +1,16 @@
 import payInvoice from "@/hooks/pay-invoice";
+import usePayLnurl from "@/hooks/pay-lnurl";
 import { useState } from "react";
 
 export const PayInvoice = () => {
-  const [invoice, setInvoice] = useState("");
-  const { data: payinvoice, loading, error, mutate } = payInvoice();
+  const [destination, setDestination] = useState("");
+  const mutationInvoice = payInvoice();
+  const mutationLnurl = usePayLnurl();
 
-  if (error) return <div>Error {error?.response?.data?.detail}</div>;
+  if (mutationInvoice.error)
+    return <div>Error {mutationInvoice.error?.response?.data?.detail}</div>;
 
-  if (loading) return <div>Loading</div>;
+  if (mutationInvoice.loading) return <div>Loading</div>;
 
   return (
     <div className="pt-4 flex gap-4 flex-col">
@@ -15,10 +18,20 @@ export const PayInvoice = () => {
       <input
         type="text"
         placeholder="invoice"
-        onChange={(e) => setInvoice(e.target.value)}
+        onChange={(e) => setDestination(e.target.value)}
       />
-      <button onClick={() => mutate(invoice)}>Pay</button>
-      <span>Payment Hash: {payinvoice?.checking_id}</span>
+      <button
+        onClick={() => {
+          if (destination.includes("@")) mutationLnurl.mutate(destination);
+          else mutationInvoice.mutate(destination);
+        }}
+      >
+        Pay
+      </button>
+      <span>
+        Payment Hash:{" "}
+        {mutationInvoice.data?.checking_id ?? mutationLnurl.data?.checking_id}
+      </span>
     </div>
   );
 };
